d885c8485ebc3563e2635503c3b1aabb64d963fc
Alex <alexandramcgarryx@gmail.com>
2024-03-01 14:49:33 +0000

    update to vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
+
+function App() {
+  return (
+    <div>
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
